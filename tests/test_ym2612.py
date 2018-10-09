8918e7cab322c3aff347dc7e336651a9012ccdf5
jimbo1qaz <jimbo1qaz@gmail.com>
2018-10-09 03:20:19 -0700

    Change YM2612 register order to (chan, op, param)

--- tests/test_ym2612.py
+++ tests/test_ym2612.py
@@ -5,13 +5,13 @@ def test_reg_pack2unpack():
     for param in range(0x30, 0x80+1, 0x10):
         for chan in range(3):
             for op in range(4):
-                pco_in = param, chan, op
+                tup_in = chan, op, param
 
-                reg = ym2612_pack(param, chan, op)
+                reg = ym2612_pack(*tup_in)
                 assert isinstance(reg, int), f'ym2612_pack returned {reg} not int'
 
-                pco_out = ym2612_unpack(reg)
-                assert pco_out == pco_in, f'Input {pco_in} != {pco_out}'
+                tup_out = ym2612_unpack(reg)
+                assert tup_out == tup_in, f'Input {tup_in} != {tup_out}'
 
 
-# TODO def test_reg_filter():
+# TODO def test_ym2612_filter():
--- vgmviz/ym2612.py
+++ vgmviz/ym2612.py
@@ -3,8 +3,8 @@ from typing import Union, TYPE_CHECKING, Callable
 if TYPE_CHECKING:
     from vgmviz.vgm import YM2612Port0, YM2612Port1
 
-
 # Parameters
+
 DetHarm = 0x30  # 4-bit detune (sign-mag), 4-bit harmonic#
 Ampl = 0x40  # 7-bit oscillator amplitude, in units of -0.75dB
 TrebAttack = 0x50  # 2-bit treble speed-boost... 5-bit attack rate
@@ -14,22 +14,22 @@ KneeRelease = 0x80  # 4-bit knee amplitude (-3dB), 4-bit release rate (,*2+1)
 
 """ YM2612 register address:
 Bit field: pppp oo cc
-Note that operators are larger than channels.
+Note that the order "feels" reversed (compare to channel, operator, param).
 """
 
 
-def ym2612_pack(param, chan, op):
-    assert param % 0x10 == 0
+def ym2612_pack(chan, op, param):
     assert 0 <= chan < 3
     assert 0 <= op < 4
-    return param + op * 4 + chan
+    assert param % 0x10 == 0
+    return param + (4 * op) + chan
 
 
 def ym2612_unpack(register):
-    param = register & 0xF0
     chan = register & 0x03
     op = (register // 4) & 0x03
-    return param, chan, op
+    param = register & 0xF0
+    return chan, op, param
 
 
 class _Wildcard:
@@ -42,11 +42,11 @@ _wildcard = _Wildcard()
 _Event = Union['YM2612Port0', 'YM2612Port1']
 
 
-def ym2612_filter(param=_wildcard, chan=_wildcard, op=_wildcard) -> \
+def ym2612_filter(chan=_wildcard, op=_wildcard, param=_wildcard) -> \
         Callable[[_Event], bool]:
-    query = (param, chan, op)
+    query = (chan, op, param)
 
     def cond(e: _Event):
-        return ym2612_unpack(e.reg) == query
+        return unpack(e.reg) == query
 
     return cond
