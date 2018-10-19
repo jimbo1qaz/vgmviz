d3d35042dc483d2c7a37048951e462b478cecd97
jimbo1qaz <jimbo1qaz@gmail.com>
2018-10-18 23:10:02 -0700

    Add modular struct encoder/decoder (both bare and command-prefixed)

---
+++ vgmviz/datastruct.py
@@ -0,0 +1,224 @@
+from typing import ClassVar, Dict, Type, Callable, Optional, Any, List, Union
+
+from dataclasses import fields, Field, dataclass, field
+
+from vgmviz.pointer import Pointer, Writer
+
+
+Args = List[Any]
+Kwargs = Dict[str, Any]
+
+
+#### DataStruct field definnitions
+
+def meta(*args, **kwargs) -> Field:
+    return field(metadata={
+        'struct_field':
+            FieldMeta(*args, **kwargs)
+    })
+
+
+def get_meta(f: Field) -> 'FieldMeta':
+    return f.metadata['struct_field']
+
+
+@dataclass
+class FieldMeta:
+    # Either: call ptr.method to read/write field
+    method: str = None  # Method called to read/write
+    arg: Optional = None  # Reading the field needs parameters (eg. magic numbers)
+    length: Optional[str] = None  # Reading the field depends on another field
+    addr: Optional[int] = None  # Read *and* write the field at a fixed address
+
+    # Or: determine field from `command - base_command`
+    parameterize: Callable[[int], int] = None
+
+    def __post_init__(self):
+        if not (self.method or self.parameterize):
+            raise ValueError(
+                'Invalid metadata, must supply one of [method, parameterize]')
+
+        if self.method and self.parameterize:
+            raise ValueError(
+                'Invalid metadata, cannot supply multiple of [method, parameterize]')
+
+
+#### DataStruct (for VGM headers)
+
+# noinspection PyDataclass
+class DataStruct:
+    """ Dataclass representing a struct. """
+    @classmethod
+    def decode(cls, ptr: Pointer) -> 'DataStruct':
+        kwargs: Kwargs = {}
+
+        for f in fields(cls):  # type: Field
+            kwargs[f.name] = struct_read(cls, ptr, f, kwargs, 0)
+
+        # noinspection PyArgumentList
+        return cls(**kwargs)
+
+    def encode(self, wrt: Writer) -> None:
+        f: Field
+        for f in fields(self):
+            value = getattr(self, f.name)
+            struct_write(self, wrt, value, f)
+
+
+#### EventStruct (special-case supporting command ID)
+
+Command = int   # int8
+cmd2event: Dict[Command, Type['EventStruct']] = {}
+
+
+def register_cmd2event(*commands: int) -> Callable:
+    """
+    Decoding lookup: cmd2event[commands] = event.
+    Encoding lookup: event.base_command = commands[0].
+    """
+
+    if len(commands) == 0:
+        raise TypeError('must supply commands to register_cmd2event')
+
+    def _register_event(event_cls: Type[EventStruct]) -> Type[EventStruct]:
+        if not issubclass(event_cls, EventStruct):
+            raise TypeError(f'{event_cls} must be Event')
+
+        event_cls.base_command = commands[0]
+        event_cls.is_multiple_commands = (len(commands) > 1)
+
+        for command in commands:
+            cmd2event[command] = event_cls
+
+        return dataclass(event_cls)
+    return _register_event
+
+
+# noinspection PyDataclass
+class EventStruct:
+    """ Event composed of command + data.
+
+    Supporting parametric events is hard.
+
+    - decode() is passed a `ptr` pointing AFTER command ID.
+    It is also passed `command_offset`
+    (used to initialize event fields, *not* saved permanently).
+        - A field can be declared `x: int = meta(parameterize=lambda x: x + 1)`
+        to initialize `x = command_offset + 1`.
+    - decode() does not read command ID from `ptr`.
+
+    - encode() checks: If `is_multiple_commands` is true,
+    it reads `command()`, which calculates command from event fields.
+    - encode() writes command ID to `wrt`, skipping all parameterized fields.
+
+    - TODO: encode() ignores fields `x` targeted by `meta(length=x)`
+    and recomputes them when writing.
+        - Only useful for binary blob editing.
+        I don't need it in the foreseeable future.
+
+    - TODO: actually define `command()` on parametric commands.
+        - I do not expect to encode() them.
+    """
+
+    base_command: ClassVar[Command]
+    command: ClassVar[Callable[[], int]]
+    is_multiple_commands: ClassVar[bool]
+
+    @classmethod
+    def decode(cls, ptr: Pointer, command: Command) -> 'EventStruct':
+        command_offset = command - cls.base_command
+        kwargs = {}
+
+        for f in fields(cls):  # type: Field
+            kwargs[f.name] = struct_read(cls, ptr, f, kwargs, command_offset)
+
+        # noinspection PyArgumentList
+        return cls(**kwargs)
+
+    # NOT classmethod
+    def encode(self, wrt: Writer) -> None:
+        if self.is_multiple_commands:
+            command = self.command()
+        else:
+            command = self.base_command
+
+        # Write command ID.
+        wrt.u8(command)
+
+        f: Field
+        for f in fields(self):
+            value = getattr(self, f.name)
+            struct_write(self, wrt, value, f)
+
+
+#### Struct field operations
+
+AnyStruct = Union[DataStruct, EventStruct]
+
+
+def struct_read(
+        cls: Type[AnyStruct],
+        ptr: Pointer,
+        f: Field,
+        kwargs: Kwargs,
+        command_offset: Optional[int]
+):
+    try:
+        metadata = get_meta(f)
+    except KeyError:
+        raise ValueError(f'broken type {cls}: field {f.name} missing metadata')
+
+    if metadata.parameterize:
+        if not getattr(cls, 'is_multiple_commands', None):
+            raise ValueError(
+                f'non-parametric {cls} cannot have parametric field {f.name}')
+        return metadata.parameterize(command_offset)
+
+    elif metadata.method:
+        ptr_args = []
+        ptr_kwargs = {}
+
+        if metadata.length:  # Data length equals another field
+            length_val = kwargs[metadata.length]
+            ptr_args.append(length_val)
+        if metadata.arg:  # Magic numbers
+            ptr_args.append(metadata.arg)
+        if metadata.addr is not None:
+            ptr_kwargs['addr'] = metadata.addr
+
+        return getattr(ptr, metadata.method)(*ptr_args, **ptr_kwargs)
+
+    else:
+        raise ValueError(
+            f'cannot decode event {cls}: field {f.name} has empty metadata')
+
+
+def struct_write(obj: AnyStruct, wrt: Writer, value: Any, f: Field) -> None:
+    cls = type(obj)
+    try:
+        metadata = get_meta(f)
+    except KeyError:
+        raise TypeError(f'broken type {cls}: field {f.name} missing metadata')
+
+    write_args = [value]
+    write_kwargs = {}
+
+    # Parametric fields are encoded via obj.command(), and do not need to be written.
+    if metadata.parameterize:
+        # assert not metadata.method
+        if not getattr(cls, 'is_multiple_commands', None):
+            raise ValueError(
+                f'non-parametric {cls} cannot have parametric field {f.name}')
+        return
+
+    elif metadata.method:
+        # Destination address
+        if metadata.addr is not None:
+            write_kwargs['addr'] = metadata.addr
+
+        # Write fields which were read from command parameters.
+        getattr(wrt, metadata.method)(*write_args, **write_kwargs)
+
+    else:
+        raise ValueError(
+            f'cannot decode event {cls}: field {f.name} has empty metadata')
--- vgmviz/vgm.py
+++ vgmviz/vgm.py
@@ -1,8 +1,9 @@
-from typing import Any, List, Callable, Type, TypeVar, Generic, Dict, ClassVar, Optional
+from typing import Any, List, Callable, Type, TypeVar, Generic
 
-from dataclasses import dataclass, field, fields, Field
+from dataclasses import dataclass, field
 
 from vgmviz import ym2612
+from vgmviz.datastruct import EventStruct, cmd2event, register_cmd2event, meta
 from vgmviz.pointer import Pointer, Writer
 
 assert ym2612
@@ -28,6 +29,8 @@ class VgmFile:
 
 
 ENDIAN = 'little'
+
+# Parse VGM
 
 def parse_vgm(path: str) -> LinearEventList:
     with open(path, 'rb') as f:
@@ -72,166 +75,9 @@ def parse_body(ptr: Pointer, file: VgmFile):
         else:
             raise VgmNotImplemented(f"Unhandled VGM command {command:#2x}")
 
-
-# Event base classes, for decoder
-
-Command = int   # int8
-cmd2event: Dict[Command, Type['Event']] = {}
-
-
-def register_cmd2event(*commands: int) -> Callable:
-    """
-    Decoding lookup: cmd2event[commands] = event.
-    Encoding lookup: event.base_command = commands[0].
-    """
-
-    if len(commands) == 0:
-        raise TypeError('must supply commands to register_cmd2event')
-
-    def _register_event(event_cls: Type[Event]) -> Type[Event]:
-        if not issubclass(event_cls, Event):
-            raise TypeError(f'{event_cls} must be Event')
-
-        event_cls.base_command = commands[0]
-        event_cls.is_multiple_commands = (len(commands) > 1)
-
-        for command in commands:
-            cmd2event[command] = event_cls
-
-        return dataclass(event_cls)
-    return _register_event
-
-
-# @dataclass
-class Event:
-    base_command: ClassVar[Command]
-
-    # A property which is defined in subclasses where base_command==True.
-    command: ClassVar[property]
-
-    # Some wait events are represented by multiple commands with 4 leading bits.
-    # The 4 trailing bits serve as a duration parameter.
-    is_multiple_commands: ClassVar[bool]
-
-    """
-    Supporting parametric events is hard.
-    
-    - decode() is passed a `ptr` pointing AFTER command ID.
-    It is also passed `command_offset`
-    (used to initialize event fields, *not* saved permanently).
-        - A field can be declared `x: int = meta(parameterize=lambda x: x + 1)`
-        to initialize `x = command_offset + 1`.
-    - decode() does not read command ID from `ptr`.
-
-    - encode() checks: If `is_multiple_commands` is true,
-    it reads the `command` property, which calculates command from event fields.
-    - encode() writes command ID to `wrt`.
-    
-    - TODO: encode() ignores fields `x` targeted by `meta(length=x)`
-    and recomputes them when writing.
-        - Only useful for binary blob editing.
-        I don't need it in the foreseeable future.
-    
-    - TODO: actually define `command` on parametric commands.
-        - I do not expect to encode() them.
-    
-    decode is a classmethod but works well as an independent function.
-    encode is a instance method.
-    """
-
-    @classmethod
-    def decode(cls, ptr: Pointer, command: Command) -> 'Event':
-        command_offset = command - cls.base_command
-        kwargs = {}
-
-        for f in fields(cls):  # type: Field
-            try:
-                meta = get_meta(f)
-            except KeyError:
-                raise TypeError(f'broken type {cls}: field {f.name} missing metadata')
-            ptr_args = []
-
-            if meta.parameterize:
-                if not cls.is_multiple_commands:
-                    raise ValueError(
-                        f'non-parametric {cls} cannot have parametric field {f.name}')
-                kwargs[f.name] = meta.parameterize(command_offset)
-
-            elif meta.method:
-                # Contrived example: ptr.bytes_(length, address)
-                if meta.length:  # length
-                    length_val = kwargs[meta.length]
-                    ptr_args.append(length_val)
-                if meta.arg:  # optional: address
-                    ptr_args.append(meta.arg)
-
-                kwargs[f.name] = getattr(ptr, meta.method)(*ptr_args)
-            else:
-                raise ValueError(
-                    f'cannot decode event {cls}: field {f.name} has empty metadata')
-
-        return cls(**kwargs)
-
-    # NOT classmethod
-    def encode(self, wrt: Writer) -> None:
-        f: Field
-
-        if self.is_multiple_commands:
-            command = self.command
-        else:
-            command = self.base_command
-
-        # Write command ID.
-        wrt.u8(command)
-
-        for f in fields(self):
-            meta = get_meta(f)
-
-            value = getattr(self, f.name)
-            write_args = [value]
-
-            # Do not write fields which were read from the command ID.
-            if meta.parameterize:
-                continue
-
-            # Write fields which were read from command parameters.
-            getattr(wrt, meta.method)(*write_args)
-
-
-@dataclass
-class FieldMeta:
-    # Either: call ptr.method to read/write field
-    method: str = None  # Method called to read/write
-    arg: Optional = None  # Reading the field needs parameters (eg. magic numbers)
-    length: Optional = None  # Reading the field depends on another field
-
-    # Or: determine field from `command - base_command`
-    parameterize: Callable[[int], int] = None
-
-    def __post_init__(self):
-        if not (self.method or self.parameterize):
-            raise ValueError(
-                'Invalid metadata, must supply one of [method, parameterize]')
-
-        if self.method and self.parameterize:
-            raise ValueError(
-                'Invalid metadata, cannot supply multiple of [method, parameterize]')
-
-
-def meta(*args, **kwargs) -> Field:
-    return field(metadata={
-        'struct_field':
-            FieldMeta(*args, **kwargs)
-    })
-
-
-def get_meta(f: Field) -> 'FieldMeta':
-    return f.metadata['struct_field']
-
-
 # Event implementations
 
-class IWait(Event):
+class IWait(EventStruct):
     delay: int
 
 
@@ -241,7 +87,7 @@ class PureWait(IWait):
 
 # PCM
 @register_cmd2event(0x67)
-class DataBlock(Event):
+class DataBlock(EventStruct):
     magic: bytes = meta('hexmagic', '66')
     typ: int = meta('u8')
     nbytes: int = meta('u32')
@@ -249,7 +95,7 @@ class DataBlock(Event):
 
 
 @register_cmd2event(0xE0)
-class PCMSeek(Event):
+class PCMSeek(EventStruct):
     address: int = meta('u32')
 
 
@@ -278,7 +124,7 @@ class Wait16Bit(PureWait):
 
 # YM2612 FM
 @dataclass
-class Write8as8(Event):
+class Write8as8(EventStruct):
     reg: int = meta('u8')
     value: int = meta('u8')
 
@@ -294,7 +140,7 @@ class YM2612Port1(Write8as8):
 
 
 @register_cmd2event(0x50)
-class PSGWrite(Event):
+class PSGWrite(EventStruct):
     value: int = meta('u8')
 
 
